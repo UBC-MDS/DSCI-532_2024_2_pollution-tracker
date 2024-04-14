2e2dd136295489968f1a623f32135ee7e3050d02
weiranzhao97 <144717444+weiranzhao97@users.noreply.github.com>
2024-04-13 21:00:52 -0700

    components.py update: debug

--- src/components.py
+++ src/components.py
@@ -74,35 +74,80 @@ def get_layout(data):
         dbc.Row(html.H1('Pollutant Tracker'), justify="center"),
         dbc.Row(pollutant_filter, justify="center"),
         dbc.Row([
-            dbc.Col(region_filter, width=4),
-            dbc.Col(country_filter, width=4),
+            dbc.Col(width=1.3), 
             dbc.Col(html.Div([
-                dbc.Row([
-                    dbc.Col(html.Div('Start Year:'), width=3),
-                    dbc.Col(start_year_dropdown, width=9)
-                ]),
-                dbc.Row([
-                    dbc.Col(html.Div('Start Month:'), width=3),
-                    dbc.Col(start_month_dropdown, width=9)
-                ]),
-                dbc.Row([
-                    dbc.Col(html.Div('End Year:'), width=3),
-                    dbc.Col(end_year_dropdown, width=9)
-                ]),
-                dbc.Row([
-                    dbc.Col(html.Div('End Month:'), width=3),
-                    dbc.Col(end_month_dropdown, width=9)
-                ])
-            ], width=4))
+                html.Label('Start Year:'),
+                start_year_dropdown
+            ]), width=2), 
+            dbc.Col(html.Div([
+                html.Label('Start Month:'),
+                start_month_dropdown
+            ]), width=2),
+            dbc.Col(width=0.7),
+            dbc.Col(html.Div(className='text-center', style={'borderLeft': '1px solid #ccc', 'height': '50px'}), width=0.7),
+            dbc.Col(html.Div([
+                html.Label('End Year:'),
+                end_year_dropdown
+            ]), width=2),  
+            dbc.Col(html.Div([
+                html.Label('End Month:'),
+                end_month_dropdown
+            ]), width=2),
+            dbc.Col(width=1.3),
         ], justify="center"),
         html.Hr(),
-        dbc.Card([
-            dbc.CardBody([
-                dcc.Graph(id='graph'),
-                dvc.Vega(id='top_countries_chart', opt={"renderer": "svg", "actions": False}, spec={}, style={'width': '100%', 'height': '100%'}),
-                dash_table.DataTable(id='data-summary-table', style_cell={'textAlign': 'center'}, style_header={'backgroundColor': 'white', 'fontWeight': 'bold'})
-            ])
-        ])
+        # First card with header: Region, Time Period Picker for Region, Graph, and Top Countries Chart
+        dbc.Card(
+            [
+                dbc.CardHeader("Worldwide Comparison", className="font-weight-bold"),  # Card header with title
+                dbc.CardBody([
+                    dbc.Row([
+                        dbc.Col(region_filter, width=3)
+                    ], justify="start"),
+                    html.Hr(), 
+                    dbc.Row([
+                        dbc.Col(graph_placeholder, width=8),
+                        dbc.Col(top_countries_chart, width=4)
+                    ], justify="around"),
+                ]),
+            ],
+            className="mb-3", 
+        ),
+        # Second card with header: Country Filter, Time Period Picker for Country, Data Summary, and Trend Chart
+        dbc.Card(
+            [
+                dbc.CardHeader("Country of Interest Trend", className="font-weight-bold"),  # Card header with title
+                dbc.CardBody([
+                    dbc.Row([
+                        dbc.Col(country_filter, width=3),
+                    ], justify="start"),
+                    html.Hr(),  
+                    dbc.Row([
+                        dbc.Col(trend_chart, width=4),
+                        dbc.Col(data_summary, width=4),
+                    ], justify="around"),
+                ]),
+            ],
+            className="mb-3",
+        ),
+        dbc.Row([
+            dbc.Col([
+                html.Div([
+                    html.P(" ", style={"font-size": "16px"}),
+                    html.P("This dashboard visualizes global air pollution levels, making it easy to explore global and local trends",
+                        style={"font-size": "16px"}),
+                    html.P("Creators: Merete Lutz, Kun Ya, Weiran Zhao, Sid Grover",
+                        style={"font-size": "12px"}),
+                    html.A("GitHub Repository", href="https://github.com/UBC-MDS/DSCI-532_2024_2_pollution-tracker",
+                        target="_blank", style={"font-size": "12px"}),
+                    html.P("Last updated on April 7, 2024",
+                        style={"font-size": "12px"}),
+                ])
+            ], width=12),
+        ]),
+        html.Div(id='dummy_output'),
+        dcc.Store(id='selected-countries', data=[]),
+        html.Div(id='first_country_name', style={'display': 'none'})
     ])
 
     return layout
