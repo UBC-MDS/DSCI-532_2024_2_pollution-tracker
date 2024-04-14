88c8ecfa46a8e11e45795985bc94d6cf3a7da2f8
Merete Lutz <meretelutz@gmail.com>
2024-04-14 10:39:42 -0700

    Change file path to be relative to src

--- src/data.py
+++ src/data.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 def load_data():
-    data = pd.read_csv("data/processed/world_air_quality.csv")
+    data = pd.read_csv("../data/processed/world_air_quality.csv")
     data['time_hour'] = pd.to_datetime(data['time_hour']).dt.tz_convert(None)
     data['time'] = pd.to_datetime(data['time']).dt.date
     return data
