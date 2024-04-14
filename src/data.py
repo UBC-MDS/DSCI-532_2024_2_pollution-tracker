ba5dae2ffb2af5cd65e4f330fcb537bddcfe2c53
weiranzhao97 <144717444+weiranzhao97@users.noreply.github.com>
2024-04-13 21:07:57 -0700

    Update data.py: debug

--- src/data.py
+++ src/data.py
@@ -1,14 +1,8 @@
 import pandas as pd
-import json
-
-def load_geojson():
-    with open("path/to/your/custom.geo.json", "r") as file:
-        geojson = json.load(file)
-    return geojson
 
 def load_data():
     data = pd.read_csv("data/processed/world_air_quality.csv")
     data['time_hour'] = pd.to_datetime(data['time_hour']).dt.tz_convert(None)
     data['time'] = pd.to_datetime(data['time']).dt.date
-    return data, load_geojson()
+    return data
 
