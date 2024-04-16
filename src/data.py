3a75682e0895a9ef7484c5e7f4acfb5636617e1d
Merete Lutz <143561866+meretelutz@users.noreply.github.com>
2024-04-16 11:37:10 -0700

    Use parquet files to speed up data loading (#142)
    * Adding parquet file to processed data
    
    * data.py updated for parquet file

--- src/data.py
+++ src/data.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
 def load_data():
-    data = pd.read_csv("../data/processed/world_air_quality.csv")
-    data['time_hour'] = pd.to_datetime(data['time_hour']).dt.tz_convert(None)
-    data['time'] = pd.to_datetime(data['time']).dt.date
+    data = pd.read_parquet('../data/processed/world_air_quality.parquet')
+    # data['time_hour'] = pd.to_datetime(data['time_hour']).dt.tz_convert(None)
+    # data['time'] = pd.to_datetime(data['time']).dt.date
     return data
 
