4314f4dc6fd7a3c00ff6715ebdf87e7dfd452fea
Kun (Carina) Ya <83109636+carinaya@users.noreply.github.com>
2024-04-16 09:46:27 -0700

    Show plots on default selection (#139)

--- src/callbacks.py
+++ src/callbacks.py
@@ -269,7 +269,9 @@ def register_callbacks(app, data):
             (data['pollutant'] == pollutant)
         ]
         if countries:
-            filtered_data = filtered_data[data['countryname'].isin(list(countries))]
+            if not isinstance(countries, list):
+                countries = [countries]
+            filtered_data = filtered_data[data['countryname'].isin(countries)]
         
         filtered_data['time'] = filtered_data['time'].astype(str)
         
@@ -330,7 +332,9 @@ def register_callbacks(app, data):
         ]
         
         if countries:
-            filtered_data = filtered_data[data['countryname'].isin(list(countries))]
+            if not isinstance(countries, list):
+                countries = [countries]
+            filtered_data = filtered_data[data['countryname'].isin(countries)]
         
         country_stats = {}
 
